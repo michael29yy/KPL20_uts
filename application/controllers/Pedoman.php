d9a28be3abe4742729f521a653b64215ad7aede3
Michael <Michael@192.168.56.1>
2020-10-22 18:48:31 +0700

    Penghapusan exit konstruk yang tidak baik

--- application/controllers/Pedoman.php
+++ application/controllers/Pedoman.php
@@ -1,5 +1,4 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
 
 class Pedoman extends CI_Controller{
 
--- application/controllers/Pengaturanprofil.php
+++ application/controllers/Pengaturanprofil.php
@@ -1,5 +1,4 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
 
 class Pengaturanprofil extends CI_Controller{
 	public function __construct(){
